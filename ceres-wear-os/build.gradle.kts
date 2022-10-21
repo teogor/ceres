8024f57f131fd06823e4df931a9b4260a1cfb144
Teodor G <grigor.teodor@gmail.com>
2022-10-21 12:36:24 +0300

    uploaded `ceres-wear-os`

---
+++ ceres-wear-os/build.gradle.kts
@@ -0,0 +1,40 @@
+import dev.teogor.ceres.Configuration
+import dev.teogor.ceres.Dependencies
+
+plugins {
+  // android
+  id("com.android.library")
+  // kotlin
+  id("kotlin-android")
+  id("kotlin-kapt")
+  id("kotlin-parcelize")
+}
+
+android {
+  namespace = "${Configuration.baseNamespace}.wear.os"
+  compileSdk = Configuration.compileSdk
+
+  defaultConfig {
+    minSdk = Configuration.minSdk
+    targetSdk = Configuration.targetSdk
+  }
+
+  compileOptions {
+    sourceCompatibility = Configuration.javaVersion
+    targetCompatibility = Configuration.javaVersion
+  }
+  kotlinOptions {
+    jvmTarget = Configuration.jvmTarget
+  }
+
+  buildFeatures {
+    dataBinding = true
+    viewBinding = true
+  }
+}
+
+dependencies {
+  implementation("androidx.core:core-ktx:1.9.0")
+  implementation("androidx.appcompat:appcompat:1.5.1")
+  implementation("com.google.android.material:material:1.6.1")
+}
