6d76b3c2bd48b1245531b422dc6643bb68470368
Teodor Grigor <open-source@teogor.dev>
2023-10-06 14:01:04 +0300

    Add lifecycle observation utilities for Activity, Application, and Composable

--- core/common/src/main/kotlin/dev/teogor/ceres/core/common/utils/LifecycleUtils.kt
+++ core/common/src/main/kotlin/dev/teogor/ceres/core/common/utils/LifecycleUtils.kt
@@ -16,10 +16,15 @@
 
 package dev.teogor.ceres.core.common.utils
 
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
@@ -46,6 +51,133 @@ fun OnLifecycleEvent(onEvent: (owner: LifecycleOwner, event: Lifecycle.Event) ->
     lifecycle.addObserver(observer)
     onDispose {
       lifecycle.removeObserver(observer)
+    }
+  }
+}
+
+/**
+ * Observe the lifecycle of the current Activity. This can be useful for performing actions when
+ * the Activity enters or exits the foreground.
+ *
+ * @param onEnterForeground Callback to execute when the Activity enters the foreground.
+ * @param onExitForeground Callback to execute when the Activity exits the foreground.
+ */
+@Composable
+fun ObserveActivityLifecycle(
+  onEnterForeground: () -> Unit = {},
+  onExitForeground: () -> Unit = {}
+) {
+  val currentActivity = activity
+
+  currentActivity?.let {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(currentActivity) {
+      val lifecycleObserver = object : DefaultLifecycleObserver {
+        override fun onStop(owner: LifecycleOwner) {
+          super.onStop(owner)
+          onExitForeground()
+        }
+
+        override fun onStart(owner: LifecycleOwner) {
+          super.onStart(owner)
+          onEnterForeground()
+        }
+      }
+
+      val lifecycle = lifecycleOwner.lifecycle
+      lifecycle.addObserver(lifecycleObserver)
+
+      onDispose {
+        lifecycle.removeObserver(lifecycleObserver)
+      }
+    }
+  }
+}
+
+/**
+ * Observe the lifecycle of the Application. This can be useful for performing actions when
+ * the Application enters or exits the foreground.
+ *
+ * @param onEnterForeground Callback to execute when the Application enters the foreground.
+ * @param onExitForeground Callback to execute when the Application exits the foreground.
+ */
+@Composable
+fun ObserveApplicationLifecycle(
+  onEnterForeground: () -> Unit = {},
+  onExitForeground: () -> Unit = {}
+) {
+  val context = LocalContext.current.applicationContext as Application
+
+  DisposableEffect(context) {
+    val observer = object : Application.ActivityLifecycleCallbacks {
+      override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        // Unused
+      }
+
+      override fun onActivityStarted(activity: Activity) {
+        // Unused
+      }
+
+      override fun onActivityResumed(activity: Activity) {
+        onEnterForeground()
+      }
+
+      override fun onActivityPaused(activity: Activity) {
+        onExitForeground()
+      }
+
+      override fun onActivityStopped(activity: Activity) {
+        // Unused
+      }
+
+      override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        // Unused
+      }
+
+      override fun onActivityDestroyed(activity: Activity) {
+        // Unused
+      }
+    }
+
+    context.registerActivityLifecycleCallbacks(observer)
+
+    onDispose {
+      context.unregisterActivityLifecycleCallbacks(observer)
+    }
+  }
+}
+
+/**
+ * Observe the lifecycle of the current Composable. This can be useful for performing actions when
+ * the Composable enters or exits the foreground.
+ *
+ * @param onEnterForeground Callback to execute when the Composable enters the foreground.
+ * @param onExitForeground Callback to execute when the Composable exits the foreground.
+ */
+@Composable
+fun ObserveComposableLifecycle(
+  onEnterForeground: () -> Unit = {},
+  onExitForeground: () -> Unit = {}
+) {
+  val lifecycleOwner = LocalLifecycleOwner.current
+
+  DisposableEffect(Unit) {
+    val lifecycleObserver = object : DefaultLifecycleObserver {
+      override fun onStop(owner: LifecycleOwner) {
+        super.onStop(owner)
+        onExitForeground()
+      }
+
+      override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        onEnterForeground()
+      }
+    }
+
+    lifecycleOwner.lifecycle.addObserver(lifecycleObserver)
+
+    onDispose {
+      lifecycleOwner.lifecycle.removeObserver(lifecycleObserver)
     }
   }
 }
