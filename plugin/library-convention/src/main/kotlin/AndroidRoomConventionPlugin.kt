658f22117da166bc9a447d4d06c4d3ca0858198b
Teodor Grigor <open-source@teogor.dev>
2023-10-27 11:25:36 +0300

    Add RoomSchemaArgProvider for Exporting Room Schemas

--- plugin/library-convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
+++ plugin/library-convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
@@ -14,16 +14,18 @@
  * limitations under the License.
  */
 
+import com.google.devtools.ksp.gradle.KspExtension
+import java.io.File
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.process.CommandLineArgumentProvider
-import java.io.File
 
 class AndroidRoomConventionPlugin : Plugin<Project> {
 
@@ -31,12 +33,12 @@ class AndroidRoomConventionPlugin : Plugin<Project> {
     with(target) {
       pluginManager.apply("com.google.devtools.ksp")
 
-      // extensions.configure<KspExtension> {
-      //   // The schemas directory contains a schema file for each version of the Room database.
-      //   // This is required to enable Room auto migrations.
-      //   // See https://developer.android.com/reference/kotlin/androidx/room/AutoMigration.
-      //   arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
-      // }
+      extensions.configure<KspExtension> {
+        // The schemas directory contains a schema file for each version of the Room database.
+        // This is required to enable Room auto migrations.
+        // See https://developer.android.com/reference/kotlin/androidx/room/AutoMigration.
+        arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
+      }
 
       val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
       dependencies {
